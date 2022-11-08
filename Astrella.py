e98ad7ae81c2438723f4ffba361ac5dd750d38c4
delionsgate <54085182+delionsgate@users.noreply.github.com>
2022-11-07 23:56:16 -0600

    Inicio del proyecto

---
+++ Astrella.py
@@ -0,0 +1,13 @@
+#Hasta ahora sólo es código a lo tonto
+def aEstrella():
+    print("HOLA XD")
+    unaLista = []
+    return unaLista
+
+def main():
+    a = aEstrella()
+    print(a)
+    a.append("Nodo1")
+    print(a)
+
+main()
